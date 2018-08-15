d4105eb1647ca4fd0b77601706b16fd4885e8212
Amitrajit Bose <amitrajitbose9@gmail.com>
2018-08-15 11:45:47 +0530

    Changed order of showing the posts and italicized the published date

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     # Query set to query published posts in the order of published dates
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
